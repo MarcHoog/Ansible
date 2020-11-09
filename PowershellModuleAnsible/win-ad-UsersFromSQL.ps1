85475d4eefb1aa74e1cf14f1a804d9346080ab07
Marc <Marc.hoogendoorn@live.nl>
2020-11-09 23:59:14 +0100

    hup

---
+++ PowershellModuleAnsible/win-ad-UsersFromSQL.ps1
@@ -0,0 +1,21 @@
+<#
+Gameplan voor dit scriptie:
+
+[!] Definier al de benodigde Input
+[!] Definier Welke Input Afhankelijk is van wie
+[!] Test De SQL server samen met de AD server
+[!] Kijk ook of je meerdere Outputs kan doen in een action
+    Out put Peruser zou opzig wel lekker zijn
+[Countertje maken voor hoeveel users het uiteindelijk zijn geweest
+Welke Al in het AD zaten en daarom Dubbel waren]
+
+
+
+
+
+
+
+
+#>
+
+#!powershell
