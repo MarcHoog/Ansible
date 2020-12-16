eb669fd86258e7c37f16a1c890021562fc9fc177
marc <Marc.hoogendoorn@live.nl>
2020-12-16 01:31:42 +0100

    ewew

--- Modules/create-user-fancy/local_deploy_user.ps1
+++ Modules/create-user-fancy/local_deploy_user.ps1
@@ -4,7 +4,8 @@
 <#
 [X] Check for Active Directory things
 [X] Check Look what kind of Action is taken to Create or to Remove A User
-[ ] Create an very simple user in Active Directory 
+[X] Create an very simple user in Active Directory 
+[] Put that User in a Certain Active Directory OU
 
 
 #>
@@ -16,42 +17,73 @@ $spec = @{
         action = @{ type = "str"; choices= "create","remove"; required = $true}
         firstname = @{ type = "str"; required = $true}
         lastname = @{ type = "str"; required = $true}
+        oupath = @{ type = "str";}
     }
 }
 
 
 $module = [Ansible.Basic.AnsibleModule]::Create($args,$spec)
 
+#Makes the Variables easier to use and more readable
+$action = $module.params.action
+$givenname = $module.params.firstname
+$surname = $module.params.lastname
+$oupath = $module.params.oupath
+
+#checks for Active directory functions
 $checkAD = Get-ADDomainController -Erroraction SilentlyContinue
 if (!$checkAD) {
         $module.failjson("Active Directory Functions aren't reachable on target computer")
 }
 
  
-if ($module.params.action -eq 'create') {
+if ($action -eq 'create') {
 
+    #Creates the command
+    $Executable_command = "New-ADUser -givenname $givenname -surname $surname "
+    
 
-    $SamAccountName = $module.params.firstname.Substring(0.5) + $module.params.lastname.Substring(0.3)
-
-    [int] $inc = 0
-    if (Get-ADuser -Filter {SamAccountName -eq "$SamAccountName"}) {    
+    #Creat An Unique Username Experience 
+    $SamAccountName = $surname.substring(0.5) + $givenname.substring(0.3)
+    if (Get-ADuser -Filter {SamAccountName -eq $SamAccountName}) {
+        [int] $inc = 0    
         do {
             $inc ++
             $SamAccountName = $SamAccountName + [string]$inc
         }
-        until (-not (Get-ADuser -Filter {SamAccountName -eq "$SamAccountName"}))   
+        until (!(Get-ADuser -Filter {SamAccountName -eq "$SamAccountName"}))   
     }
-    
+
+    $Executable_command += "-name $SamAccountName -SamAccountName $SamAccountName"
+
+    #Checks for OU path
+    $checkoupath = Get-ADOrganizationalUnit -Identity $oupath | select-object Distinguishedname -ErrorAction SilentlyContinue
+    if($checkoupath){
+        $Executable_command += "-path $oupath "
+    }else {
+        $module.warningjson("Couldn't find $oupath user $SamAccountName will be placed in default Directory")
+    }
+
+
+
+
+
+
+
+
+
+    #The Command that Actually Pushes the user
     New-ADUser `
         -Name $SamAccountName `
-        -givenname $module.params.firstname `
-        -surname  $module.params.lastname `
-        -SamAccountname $SamAccountName
+        -givenname `
+         `
+        -SamAccountname $SamAccountName `
+        -path $module.params.oupath
 }
 
 
 
-elseif ($module.params.action -eq 'remove') {    
+elseif ($action -eq 'remove') {    
     
 }
 
